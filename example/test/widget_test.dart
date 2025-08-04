54d5ffd5639f053b26cc205f4f19c4c23efcba6f
stas.bozhko <stas.bozhko@gmail.com>
2025-08-04 16:40:00 +0200

    Fix widget test import and run analyze/format

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:web_validator/main.dart';
+import 'package:example/main.dart';
 
 void main() {
   testWidgets('Email validator app smoke test', (WidgetTester tester) async {
